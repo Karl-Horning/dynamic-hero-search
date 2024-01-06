44605d1406aedd0889a3b6513ec5eadcb67de8e5
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-01-06 18:34:44 +0000

    Refactored code

--- src/js/script.js
+++ src/js/script.js
@@ -1,97 +1,105 @@
-const superheroes = [
-    {
-        name: "Batman",
-        publisher: "DC Comics",
-    },
-    {
-        name: "Superman",
-        publisher: "DC Comics",
-    },
-    {
-        name: "Wonder Woman",
-        publisher: "DC Comics",
-    },
-    {
-        name: "Flash",
-        publisher: "DC Comics",
-    },
-    {
-        name: "Green Lantern",
-        publisher: "DC Comics",
-    },
-    {
-        name: "Spider-Man",
-        publisher: "Marvel",
-    },
-    {
-        name: "Hulk",
-        publisher: "Marvel",
-    },
-    {
-        name: "Iron Man",
-        publisher: "Marvel",
-    },
-    {
-        name: "Captain America",
-        publisher: "Marvel",
-    },
-    {
-        name: "Captain Carter",
-        publisher: "Marvel",
-    },
-    {
-        name: "The Punisher",
-        publisher: "Marvel",
-    },
-    {
-        name: "Deadpool",
-        publisher: "Marvel",
-    },
-    {
-        name: "Gwenpool",
-        publisher: "Marvel",
-    },
-];
+(() => {
+    const superheroes = [
+        {
+            name: "Batman",
+            publisher: "DC Comics",
+        },
+        {
+            name: "Superman",
+            publisher: "DC Comics",
+        },
+        {
+            name: "Wonder Woman",
+            publisher: "DC Comics",
+        },
+        {
+            name: "Flash",
+            publisher: "DC Comics",
+        },
+        {
+            name: "Green Lantern",
+            publisher: "DC Comics",
+        },
+        {
+            name: "Spider-Man",
+            publisher: "Marvel",
+        },
+        {
+            name: "Hulk",
+            publisher: "Marvel",
+        },
+        {
+            name: "Iron Man",
+            publisher: "Marvel",
+        },
+        {
+            name: "Captain America",
+            publisher: "Marvel",
+        },
+        {
+            name: "Captain Carter",
+            publisher: "Marvel",
+        },
+        {
+            name: "The Punisher",
+            publisher: "Marvel",
+        },
+        {
+            name: "Deadpool",
+            publisher: "Marvel",
+        },
+        {
+            name: "Gwenpool",
+            publisher: "Marvel",
+        },
+    ];
 
-const searchBox = document.getElementById("search-box");
-const searchGroup = document.getElementById("search-group");
-const heroTableBody = document.getElementById("hero-table-body");
+    const searchBox = document.getElementById("search-box");
+    const searchGroup = document.getElementById("search-group");
+    const heroTableBody = document.getElementById("hero-table-body");
 
-function preventSubmit() {
-    return false; // Returning false prevents the form from submitting
-}
-
-function createSuperheroList(superheroes) {
-    superheroes.forEach((superhero) => {
+    const createTableRow = (superhero) => {
         const tableRow = document.createElement("tr");
-        tableRow.innerHTML = `<td>${superhero.name}</td><td>${superhero.publisher}</td>`;
-        heroTableBody.appendChild(tableRow);
-    });
-}
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        td1.textContent = superhero.name;
+        td2.textContent = superhero.publisher;
+        tableRow.appendChild(td1);
+        tableRow.appendChild(td2);
+        return tableRow;
+    };
 
-function filterSearch(e) {
-    const outputList = superheroes.filter((superhero) => {
-        return (
-            superhero.name
-                .toLowerCase()
-                .includes(searchBox.value.toLowerCase()) ||
-            superhero.publisher
-                .toLowerCase()
-                .includes(searchBox.value.toLowerCase())
+    const createSuperheroList = (superheroes) => {
+        superheroes.forEach((superhero) => {
+            const tableRow = createTableRow(superhero);
+            heroTableBody.appendChild(tableRow);
+        });
+    };
+
+    const filterSearch = () => {
+        const outputList = superheroes.filter((superhero) => {
+            return (
+                superhero.name
+                    .toLowerCase()
+                    .includes(searchBox.value.toLowerCase()) ||
+                superhero.publisher
+                    .toLowerCase()
+                    .includes(searchBox.value.toLowerCase())
+            );
+        });
+        heroTableBody.innerHTML = "";
+        createSuperheroList(outputList);
+    };
+
+    searchBox.addEventListener("keyup", filterSearch);
+
+    searchGroup.addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+
+    window.addEventListener("load", () => {
+        createSuperheroList(
+            superheroes.sort((a, b) => a.name.localeCompare(b.name))
         );
     });
-    heroTableBody.innerHTML = "";
-    createSuperheroList(outputList);
-}
-
-searchBox.addEventListener("keyup", filterSearch);
-
-searchGroup.addEventListener("submit", (e) => {
-    e.preventDefault();
-});
-
-window.onload = createSuperheroList(
-    superheroes.sort((a, b) => {
-        return a.name.localeCompare(b.name);
-    })
-);
+})();
